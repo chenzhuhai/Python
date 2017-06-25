906c1a2de5810a28ef87439c66a3673d2927b1d6
chenzhuhai <403426459@qq.com>
2017-06-25 13:04:35 +0800

    add gogo

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ cha.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
     print("[消费者] recv %s" % body)
-
+gogo
 cha.basic_consume(callback, queue='hello', no_ack=True)
 print('[消费者] waiting for msg.')
 cha.start_consuming()

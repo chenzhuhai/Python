265460edc0a299f3efc462bb4933897ef12d151f
chenzhuhai <403426459@qq.com>
2017-06-25 12:35:19 +0800

    python: create python project

---
+++ consumer.py
@@ -0,0 +1,13 @@
+import pika
+import sys
+
+conn = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=pika.PlainCredentials('czh', 'czh')))
+cha  = conn.channel()
+cha.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print("[消费者] recv %s" % body)
+
+cha.basic_consume(callback, queue='hello', no_ack=True)
+print('[消费者] waiting for msg.')
+cha.start_consuming()
